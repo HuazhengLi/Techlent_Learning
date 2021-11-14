26bb2f0d8e03b251d247ff5366d311a1c6fa700f
李华征 <lihuazheng@lihuazhengdeMacBook.local>
2021-11-13 19:53:51 -0500

    add new files

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from calculator import Calculator
+cal = Calculator()
+
+print(cal.add(10,5))
+print(cal.subtract(10,5))
+print(cal.multiply(10,5))
+print(cal.divide(10,5))
+print(cal.divide(10,0))
+
